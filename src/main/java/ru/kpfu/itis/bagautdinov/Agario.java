3bce141e466e6d6b91f440ba7b232457891fff62
Daniil <daniilbagautdinov@yandex.ru>
2021-12-05 20:11:53 +0300

    added application class

---
+++ src/main/java/ru/kpfu/itis/bagautdinov/Agario.java
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.bagautdinov;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Agario extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
