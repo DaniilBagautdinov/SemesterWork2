f43b57732886064d6fb811729599b2008b0abc32
Daniil <daniilbagautdinov@yandex.ru>
2021-12-12 00:08:18 +0300

    added logic

--- src/main/java/ru/kpfu/itis/bagautdinov/client/Game.java
+++ src/main/java/ru/kpfu/itis/bagautdinov/client/Game.java
@@ -1,17 +1,19 @@
 package ru.kpfu.itis.bagautdinov.client;
 
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.Random;
 
 public class Game {
 
-    private Random random = new Random();
     private char lastWinner = 'X';
 
     public void createWindow(Stage primaryStage) {
@@ -19,33 +21,131 @@ public class Game {
         root.setId("root");
         HBox top = new HBox();
         top.setId("top");
-        Button newGame = new Button("Новая игра");
+        Button newGame = new Button("New Game");
         newGame.setId("newGame");
-        Button quit = new Button("Выход");
+        Button quit = new Button("Quit");
         quit.setId("quit");
 
         GridPane grid = new GridPane();
         grid.setId("grid");
+        Label resultLabel = new Label();
+        Label winnerLabel = new Label();
+        Button[][] buttons = new Button[3][3];
 
+        getWinner(grid, buttons, winnerLabel, resultLabel);
+
+        newGame.setOnAction(e -> {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    buttons[i][j].setText("");
+                    buttons[i][j].setId("empty");
+                }
+            }
+            if (lastWinner == 'O') {
+                botLogic(buttons);
+            }
+            resultLabel.setId("");
+            resultLabel.setText("");
+            winnerLabel.setId("");
+            winnerLabel.setText("");
+        });
+
+        quit.setOnAction(e -> System.exit(0));
+
+        newGame.setPrefWidth(100);
+        HBox.setMargin(newGame, new Insets(10, 0, 10, 15));
+        quit.setPrefWidth(100);
+        HBox.setMargin(quit, new Insets(10, 0, 10, 20));
+        VBox.setMargin(grid, new Insets(20, 20, 20, 20));
+        top.getChildren().addAll(newGame, quit);
+        winnerLabel.setAlignment(Pos.CENTER);
+        winnerLabel.setPrefWidth(70);
+        VBox.setMargin(winnerLabel, new Insets(0, 0, 0, 90));
+        resultLabel.setPrefWidth(70);
+        resultLabel.setPrefHeight(70);
+        resultLabel.setAlignment(Pos.CENTER);
+        VBox.setMargin(resultLabel, new Insets(0, 0, 0, 90));
+        root.getChildren().addAll(top, grid, winnerLabel, resultLabel);
+        primaryStage.setTitle("Tictactoe");
+        Scene scene = new Scene(root, 250, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 
+    private void getWinner(GridPane grid, Button[][] buttons, Label winnerLabel, Label resultLabel) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int x = i, y = j;
+                buttons[i][j] = new Button("");
+                grid.add(buttons[i][j], j, i);
+                buttons[i][j].setPrefWidth(70);
+                buttons[i][j].setPrefHeight(70);
 
-    private void botLogic(Button[][] buttons) {
-        int i, j = 0;
-        int xCount = 0, oCount = 0, count = 0;
+                buttons[i][j].setOnAction(e -> {
+                    int xCount = 0, oCount = 0;
+                    if (resultLabel.getText().equals("")) {
 
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
-                if (buttons[i][j].getText().equals("O"))
-                    count++;
+                        if (lastWinner == 'X') {
+                            for (int a = 0; a < 3; a++) {
+                                for (int b = 0; b < 3; b++) {
+                                    if (buttons[a][b].getText().equals("X"))
+                                        xCount++;
+                                }
+                            }
+
+                            if (buttons[x][y].getText().equals("")) {
+                                buttons[x][y].setText("X");
+                                buttons[x][y].setId("X");
+                                xCount++;
+                                checkDraw(buttons, winnerLabel, resultLabel, xCount);
+
+                                if (winnerLabel.getText().equals(""))
+                                    botLogic(buttons);
+                                checkWinner(buttons, winnerLabel, resultLabel);
+                            }
+                        } else {
+                            for (int a = 0; a < 3; a++) {
+                                for (int b = 0; b < 3; b++) {
+                                    if (buttons[a][b].getText().equals("O"))
+                                        oCount++;
+                                }
+                            }
+
+                            if (buttons[x][y].getText().equals("")) {
+                                buttons[x][y].setText("X");
+                                buttons[x][y].setId("X");
+                            }
+                            checkWinner(buttons, winnerLabel, resultLabel);
+
+                            if (winnerLabel.getText().equals("")) {
+                                botLogic(buttons);
+                                oCount++;
+                            }
+                            checkDraw(buttons, winnerLabel, resultLabel, oCount);
+                        }
+                    }
+                });
             }
         }
-
-
     }
 
-    public static String check(Button[][] buttons) {
+    private void checkWinner(Button[][] buttons, Label winnerLabel, Label resultLabel) {
+        if (!checkCombinations(buttons).equals("")) {
+            getResult(winnerLabel, resultLabel, checkCombinations(buttons));
+        }
+    }
+
+    private void checkDraw(Button[][] buttons, Label winnerLabel, Label resultLabel, int xCount) {
+        if (!checkCombinations(buttons).equals("")) {
+            getResult(winnerLabel, resultLabel, checkCombinations(buttons));
+        } else if (xCount == 5) {
+            winnerLabel.setId("winner");
+            winnerLabel.setText("Draw");
+        }
+    }
+
+    public static String checkCombinations(Button[][] buttons) {
         for (int i = 0; i < 3; i++) {
             if (buttons[i][0].getText().equals(buttons[i][1].getText())
                     && buttons[i][1].getText().equals(buttons[i][2].getText())
@@ -63,5 +163,34 @@ public class Game {
                 && !buttons[1][1].getText().equals(""))
             return (buttons[1][1].getText());
         return ("");
+    }
+
+
+    private void getResult(Label winnerLabel, Label resultLabel, String winner) {
+        winnerLabel.setId("winner");
+        winnerLabel.setText("Victory");
+        resultLabel.setId(winner);
+        resultLabel.setText(winner);
+        switch (winner) {
+            case "X":
+                lastWinner = 'X';
+                break;
+            case "O":
+                lastWinner = 'O';
+        }
+    }
+
+    private void botLogic(Button[][] buttons) {
+        int i, j = 0;
+        int xCount = 0, oCount = 0, count = 0;
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().equals("O"))
+                    count++;
+            }
+        }
+
+
     }
 }
